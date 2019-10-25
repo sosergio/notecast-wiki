73ef775dd81ae72a2974ba3a2e0921b474f66b84
Sergio Sorrentino <sergio.sorrentino@ihsmarkit.com>
2019-10-25 15:50:49 +0100

    app has technologies with data

--- src/screens/TechnologiesScreen.js
+++ src/screens/TechnologiesScreen.js
@@ -4,6 +4,11 @@ import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 const useStyles = makeStyles(theme => ({  
   sidebarAboutBox: {
@@ -25,9 +30,48 @@ function TechnologiesScreen() {
             Technologies
           </Typography>
           <Divider />
+          <br/>
+          <Paper>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Area</TableCell>
+                  <TableCell>Experience</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow key="1">
+                  <TableCell>Javascript</TableCell>
+                  <TableCell>Programming Language</TableCell>
+                  <TableCell>High Profiency</TableCell>
+                </TableRow>
+                <TableRow key="3">
+                  <TableCell>React Native</TableCell>
+                  <TableCell>Framework</TableCell>
+                  <TableCell>New</TableCell>
+                </TableRow>
+                <TableRow key="4">
+                  <TableCell>Expo</TableCell>
+                  <TableCell>Mobile SDK</TableCell>
+                  <TableCell>New</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Paper>
+
           <Typography className={classes.mainContentParagraph}>
-          available doing the same thing with annotated screen captures if possible
-          available doing the same thing with annotated screen captures if possible
+          <b>React Native</b> is a framework that allows us to build native (iOS and Android) apps using the React JavaScript framework. 
+          While React Native also permits development in native languages for Android and iOS, Expo, on the other hand, only permits the use of purely JavaScript and React 
+          for the purpose of development, and provides the Software Development Kit (SDK) and the build environment so that we can only use the provided modules for native development. 
+          </Typography>
+          <Typography className={classes.mainContentParagraph}>
+          Recently, with the release of React Native for Web, React Native has introduced a new addition to its supporting platforms. 
+          Expo, in accordance with the current trend, has started supporting the beta version of Expo Web with the release of Expo SDK v33.0.0 in June. 
+          Expo Web enables the implementation of a mobile Web and also support Progressive Web Apps (PWA). 
+          This means that the components provided by React Native and Expo can not only be built for the native app, 
+          but also can be executed on the web using the same components. 
+          As is true for React, ReactDom is used for rendering on the web.
           </Typography>
         </Grid>
         <Grid item xs={12} md={4}>

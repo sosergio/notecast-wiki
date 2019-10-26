6acca9daaf7057349582e45eaf09e2b0f0eb649f
Sergio Sorrentino <sosergio@gmail.com>
2019-10-26 21:11:35 +0100

    refactor image folders

--- src/components/competitorsData.js
+++ src/components/competitorsData.js
@@ -2,70 +2,70 @@ export default [{
     id: 1,
     group: 'overcast',
     title: 'Home',
-    img: '/competitors/overcast_home.png',
+    img: '/overcast_home.png',
     description: 'something is fishy'
   },
   {
     id: 2,
     group: 'overcast',
     title: 'Search',
-    img: '/competitors/overcast_search.png',
+    img: '/overcast_search.png',
     description: 'something is fishy'
   },
   {
     id: 3,
     group: 'overcast',
     title: 'Detail',
-    img: '/competitors/overcast_detail.png',
+    img: '/overcast_detail.png',
     description: 'something is fishy'
   },
   {
     id: 4,
     group: 'overcast',
     title: 'Playing',
-    img: '/competitors/overcast_playing.png',
+    img: '/overcast_playing.png',
     description: 'something is fishy'
   },
   {
     id: 5,
     group: 'overcast',
     title: 'Downloaded',
-    img: '/competitors/overcast_downloaded.png',
+    img: '/overcast_downloaded.png',
     description: 'something is fishy'
   },
   {
     id: 6,
     group: 'apple',
     title: 'Home',
-    img: '/competitors/apple_home.png',
+    img: '/apple_home.png',
     description: 'something is fishy'
   },
   {
     id: 7,
     group: 'apple',
     title: 'Search',
-    img: '/competitors/apple_search.png',
+    img: '/apple_search.png',
     description: 'something is fishy'
   },
   {
     id: 8,
     group: 'apple',
     title: 'Detail',
-    img: '/competitors/apple_detail.png',
+    img: '/apple_detail.png',
     description: 'something is fishy'
   },
   {
     id: 9,
     group: 'apple',
     title: 'Playing',
-    img: '/competitors/apple_playing.png',
+    img: '/apple_playing.png',
     description: 'something is fishy'
   },
   {
     id: 10,
     group: 'apple',
     title: 'Downloaded',
-    img: '/competitors/apple_downloaded.png',
+    img: '/apple_downloaded.png',
     description: 'something is fishy'
   }
 ];
